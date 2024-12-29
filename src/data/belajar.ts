5985d7d3aff65d15466c89ff48eb9a926a015d59
ckckckcz <rriovld@gmail.com>
2024-12-29 14:32:00 +0700

    update: add belajarData model and implement Card component for educational content display

---
+++ src/data/belajar.ts
@@ -0,0 +1,68 @@
+import type { belajarData } from '../model/belajarData';
+
+export const card: belajarData[] = [
+    {
+        id: "1",
+        image: "https://i.pinimg.com/originals/b4/87/58/b4875898432c33be4b57c409b662db11.png",
+        alt: "Tari Adat",
+        title: "Tari Adat Tradisional Indonesia",
+        description: "Pelajari berbagai jenis tari adat dari berbagai daerah di Indonesia. Dari tari Saman Aceh hingga tari Kecak Bali.",
+        link: "https://quizizz.com/admin/quiz/6021a2345e7ccf001bda16da/keragaman-pakaian-dan-tari-adat"
+    },
+    {
+        id: "2",
+        image: "https://asset.kompas.com/crops/k1bNAjvuMqiZHgTY62DcaqZjBhE=/6x0:800x529/375x240/data/photo/2022/09/08/63196219761db.jpg",
+        alt: "Seni Tradisional",
+        title: "Mengenal Seni Tradisional Indonesia",
+        description: "Bagian tak terpisahkan dari identitas budaya bangsa yang kaya akan nilai sejarah dan makna mendalam.",
+        link: "https://quizizz.com/admin/quiz/5d89a5589e5d32001a7bf811/latihan-pts-seni-budaya"
+    },
+    {
+        id: "3",
+        image: "https://fatek.umsu.ac.id/wp-content/uploads/2023/06/Candi-Borobudur-Makna-Yang-Terkandung-di-Dalamnya.jpg",
+        alt: "Candi Borobudur",
+        title: "Sejarah Candi Borobudur",
+        description: "Temukan keajaiban dunia di Indonesia dengan memahami sejarah, makna, dan keindahan arsitektur Candi Borobudur.",
+        link: "https://id.wikipedia.org/wiki/Borobudur"
+    },
+    {
+        id: "4",
+        image: "https://cdn0-production-images-kly.akamaized.net/DdoBXYLBlvTG1epEtUyAQbFpLsM=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3126596/original/024930300_1589347291-masakan-indonesia-featured-212892.jpg",
+        alt: "Kuliner Nusantara",
+        title: "Kuliner Tradisional Nusantara",
+        description: "Jelajahi rasa kuliner nusantara autentik Indonesia melalui berbagai kuliner khas dari Sabang hingga Merauke.",
+        link: "https://www.goodnewsfromindonesia.id/2021/10/13/10-makanan-khas-indonesia"
+    },
+    {
+        id: "5",
+        image: "https://asset.kompas.com/crops/dV7e_PU99wIEX4r63Ta97yyMh-Y=/0x8:613x417/1200x800/data/photo/2022/08/07/62ef8bae4145f.jpg",
+        alt: "Pakaian Adat",
+        title: "Pakaian Adat Daerah di Indonesia",
+        description: "Kenali keunikan pakaian adat dari berbagai daerah yang mencerminkan budaya lokal dan warisan leluhur.",
+        link: "https://www.kompas.com/skola/read/2021/08/17/080000469/mengenal-pakaian-adat-indonesia"
+    },
+    {
+        id: "6",
+        image: "https://fatek.umsu.ac.id/wp-content/uploads/2023/06/Candi-Borobudur-Makna-Yang-Terkandung-di-Dalamnya.jpg",
+        alt: "Candi Borobudur",
+        title: "Sejarah Candi Borobudur",
+        description: "Temukan keajaiban dunia di Indonesia dengan memahami sejarah, makna, dan keindahan arsitektur Candi Borobudur.",
+        link: "https://id.wikipedia.org/wiki/Borobudur"
+    },
+    {
+        id: "7",
+        image: "https://cdn0-production-images-kly.akamaized.net/DdoBXYLBlvTG1epEtUyAQbFpLsM=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3126596/original/024930300_1589347291-masakan-indonesia-featured-212892.jpg",
+        alt: "Kuliner Nusantara",
+        title: "Kuliner Tradisional Nusantara",
+        description: "Jelajahi rasa kuliner nusantara autentik Indonesia melalui berbagai kuliner khas dari Sabang hingga Merauke.",
+        link: "https://www.goodnewsfromindonesia.id/2021/10/13/10-makanan-khas-indonesia"
+    },
+    {
+        id: "8",
+        image: "https://asset.kompas.com/crops/dV7e_PU99wIEX4r63Ta97yyMh-Y=/0x8:613x417/1200x800/data/photo/2022/08/07/62ef8bae4145f.jpg",
+        alt: "Pakaian Adat",
+        title: "Pakaian Adat Daerah di Indonesia",
+        description: "Kenali keunikan pakaian adat dari berbagai daerah yang mencerminkan budaya lokal dan warisan leluhur.",
+        link: "https://www.kompas.com/skola/read/2021/08/17/080000469/mengenal-pakaian-adat-indonesia"
+    },
+];
---
+++ src/model/belajarData.d.ts
@@ -0,0 +1,8 @@
+export interface belajarData {
+    id: string;
+    image: string;
+    alt: string;
+    title: string;
+    description: string;
+    link: string;
+}  
